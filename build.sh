fa23b94cc2be467bcfdb51b357e985a3e40b99c9
Zeno Zeng <zenoofzeng@gmail.com>
2014-11-27 21:39:02 +0800

    resume.html -> index.html

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 rm -f node_modules/markdown-resume/assets/css/*
 cp *.css node_modules/markdown-resume/assets/css/
 node node_modules/markdown-resume/bin/md2resume resume.md
+mv resume.html index.html
 # node node_modules/markdown-resume/bin/md2resume --pdf resume.md
